23fbcd0d62a660a7edb03e76d88b5b4b2c51a26b
Ben Cripps <ocordu@gmail.com>
2016-03-18 15:18:49 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>COMP_SCI[1]</title>
+		<link href = "https://fonts.googleapis.com/css?family=Pathway+Gothic+One" rel = "stylesheet" type = "text/css">
+		<link rel = "stylesheet" type = "text/css" href = "style.css">
+	</head>
+	<body>
+		<div id = "container">
+			<div id = "header">
+				<div id = "logo">
+					<img src = "logo.svg">
+				</div>
+				<div id = "title">
+					COMP_SCI[1]
+				</div>
+			</div>
+			<div id = "content">
+				Content.
+			</div>
+		</div>
+	</body>
+</html>
