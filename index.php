186df015c8c815edcf5fe1af51de99f1d542a364
Ben Cripps <ocordu@gmail.com>
2016-03-18 19:34:18 +0000

    PHP proof-of-concept

--- index.php
+++ index.php
@@ -15,8 +15,23 @@
 					COMP_SCI[1]
 				</div>
 			</div>
-			<div id = "content">
-				Content.
+			<div id = "content_container">
+				<div class = "content_title">
+					Random binary numbers generated with PHP (normally I would recommend JS for something like this; this is a proof of concept)
+				</div>
+				<?php
+					function echoRandomBinary() {
+						$length = mt_rand(100, 1000);
+						for ($i = 0; $i < $length; $i++) {
+							echo mt_rand(0, 1);
+						}
+					}
+					for ($i = 0; $i < 5; $i++) {
+						echo "<div class = \"content\">";
+						echoRandomBinary();
+						echo "</div>";
+					}
+				?>
 			</div>
 		</div>
 	</body>
