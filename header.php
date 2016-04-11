66bb990b1365c1f79af52a1b8eaf3ff7cb64817f
warren0797 <warreneagles@yahoo.co.uk>
2016-04-11 16:31:21 +0100

    Added navbar links

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div id="header">
     <div id="logo"><img src="logo.svg"></div>
     <div id="website_name">COMP_SCI[1]</div>
-    <div id="links"><a href="profiles.php"> test</a> <a href="health-and-safety.php"> test</a> </div>
+    <div id="links"><a href="profiles.php">Profiles</a> <a href="health-and-safety.php">H&amp;S</a></div>
 </div>
