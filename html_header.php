9488388ba5b8d8099e68e0c6ffa8c06c400417e6
ProgDroid <fm_ferreira1996@hotmail.com>
2016-04-11 15:59:19 +0100

    w

---
+++ html_header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>COMP_SCI[1]</title>
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pathway+Gothic+One">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
--- index.php
+++ index.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>COMP_SCI[1]</title>
-		<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Pathway+Gothic+One">
-		<link rel="stylesheet" type="text/css" href="style.css">
-	</head>
+<?php
+	include("html_header.php");
+?>
 	<body>
 		<div id="container">
 			<div id="header">
