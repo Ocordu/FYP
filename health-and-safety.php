860eb5b90cec0bf9ffbf04dac999b3c9c6e9e1f1
warren0797 <warreneagles@yahoo.co.uk>
2016-04-13 01:00:52 +0100

    Added content for h&s, fixed bullet points to be inside divs

--- health-and-safety.php
+++ health-and-safety.php
@@ -11,10 +11,24 @@
 			<div id="content_container">
 				<div class="padder">
 					<div class="content_title">
-						Health and Safety
+						<u>Health and Safety</u>
+					</div>
+					<div class="content_title">
+						There are several rules which should be followed when working with computers for either extended or short periods of time:
 					</div>
 					<div class="content">
-						Apply common sense.
+						<ul>
+							<li>Food and drink should be consumed and stored well away from computers.</li>
+							<li>Any loose wires should be concealed to avoid damage or possible removal from sockets.</li>
+							<li>Regular 5-10 minute breaks should be taken when using a computer for an extended period of time.</li>
+							<li>Any vents should be left uncovered to avoid overheating.</li>
+							<li>Good posture, ideally with a straight back, should be maintained at all times.</li>
+							<li>Adjust the position of the keyboard and mouse to suit comfort and avoid wrist or arm discomfort.</li>
+							<li>Adjust the position of the monitor so the top of the screen is at approximately eye level, to avoid neck or shoulder discomfort.</li>
+							<li>Ensure that the working environment is comfortable eg: lighting and temperature.</li>
+							<li>Ensure glasses or equivalent are being worn while working and the screen is free from flickering.</li>
+							<li>Ensure that the screen is not too bright or too dark.</li>
+						</ul>
 					</div>
 				</div>
 			</div>
