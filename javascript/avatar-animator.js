eee29de027671289c903d82e0066dea17b61df01
Ben Cripps <ocordu@gmail.com>
2016-04-19 06:05:08 +0100

    Awesome avatars.

--- javascript/avatar-animator.js
+++ javascript/avatar-animator.js
@@ -10,20 +10,30 @@ avatarSelector.each(function() {
 });
 function animateAvatar(avatar, animation) {
 	var duration = 200;
-	var brightness = "100%";
+	var newBlur = "0px";
+	var newBrightness = "100%";
+	var newGrayscale = "0%";
+	var newDivScale = 1;
+	var newImageScale = 1;
 	if (animation == "enter") {
-		brightness = "35%";
+		newBlur = "3px";
+		newBrightness = "85%";
+		newGrayscale = "100%";
+		newDivScale = 1.05;
+		newImageScale = 1.03;
 	}
+	$(avatar).transition({
+		scale : newDivScale
+	}, duration);
 	$(avatar).find("img").transition({
-		"-webkit-filter" : "brightness(" + brightness + ")"
-	}, duration, function() {
-	});
+		"-webkit-filter" : "blur(" + newBlur + ") brightness(" + newBrightness + ") grayscale(" + newGrayscale + ")",
+		scale : newImageScale
+	}, duration);
 	var newTextPosition = "0vw";
 	if (animation == "enter") {
 		newTextPosition = "-5vw";
 	}
 	$(avatar).find(".avatar_text_container").find("span").transition({
 		y : newTextPosition
-	}, duration, function() {
-	});
+	}, duration);
 }
