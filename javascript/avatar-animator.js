3d42dc92d5bd34043f07da629e7da7394fb4190c
Ben <ocordu@gmail.com>
2016-04-19 01:57:18 +0100

    Fixed minor avatar bug, improved responsiveness.

--- javascript/avatar-animator.js
+++ javascript/avatar-animator.js
@@ -2,15 +2,23 @@ var avatarSelector = $(".avatar");
 avatarSelector.each(function() {
 	var currentAvatar = $(this);
 	currentAvatar.mouseenter(function() {
-		animateAvatarText(currentAvatar, "-");
+		animateAvatar(currentAvatar, "-");
 	});
 	currentAvatar.mouseleave(function() {
-		animateAvatarText(currentAvatar, "+");
+		animateAvatar(currentAvatar, "+");
 	});
 });
-function animateAvatarText(avatar, sign) {
+function animateAvatar(avatar, sign) {
+	var brightness = "100%";
+	if (sign == "-") {
+		brightness = "35%";
+	}
+	$(avatar).find("img").transition({
+		"-webkit-filter" : "brightness(" + brightness + ")"
+	}, 250, function() {
+	});
 	$(avatar).find(".avatar_text_container").find("span").transition({
-		y : sign + "100px"
+		y : sign + "5vw"
 	}, 250, function() {
 	});
 }
