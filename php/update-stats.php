6b5a0972984f7b55651779d6ee4cbb86a2f3dab4
Ben <ocordu@gmail.com>
2016-05-04 02:52:30 +0100

    Added update-stats.php

---
+++ php/update-stats.php
@@ -0,0 +1,3 @@
+<?php
+	header("Authorization: Basic 4MTU4EHH6RKQXZYEMK04");
+	$data = file_get_contents("https://4MTU4EHH6RKQXZYEMK04:killme@data.unistats.ac.uk/api/v3/KIS/Institution/10007798/Course/242/FullTime/Statistics.XML");
