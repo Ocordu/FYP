9edf66af8b14d56b8b270b5093284afb6030d4a0
Ben <ocordu@gmail.com>
2016-05-04 05:43:30 +0100

    nah

--- javascript/fact-loader.js
+++ javascript/fact-loader.js
@@ -1,18 +1,8 @@
-$.ajax({
-	type: "GET",
-	url: "data/stats/course.xml",
-	dataType: "xml"
-}).success(function(data) {
-	$(data).find("Statistic").each(function() {
-		alert($(this).find("Code").text());
-		if ($(this).find("Code").text() == "DEGREECLASS") {
-			$(this).find("Details").find("StatisticDetail").each(function() {
-				if ($(this).find("Code") == "UFIRST") {
-					var percent = $(this).find("Value").text();
-					alert("yes");
-					$("#cs_facts").html(percent + "% achieved a first.");
-				}}
-			);
+$.get("data/stats/course.xml").success(function(data) {
+	$(data).find("Statistic").each(function(index, object) {
+		alert($(object).find("Code").text());
+		if ($(object).find("Code").text() == "DEGREECLASS") {
+			alert($(object).find("Code").text());
 		}
 	});
 });
