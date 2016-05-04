bd069d09314f05b2239c1cfa199cdf7ae5fe70ef
ProgDroid <fm_ferreira1996@hotmail.com>
2016-05-04 03:14:37 +0100

    Moare php to save data to file

--- php/update-stats.php
+++ php/update-stats.php
@@ -1,3 +1,7 @@
 <?php
 	header("Authorization: Basic 4MTU4EHH6RKQXZYEMK04");
 	$data = file_get_contents("https://4MTU4EHH6RKQXZYEMK04:killme@data.unistats.ac.uk/api/v3/KIS/Institution/10007798/Course/242/FullTime/Statistics.XML");
+	$stats = fopen("../data/stats.jpg", "w");
+	fwrite($stats, $data);
+	fclose($stats);
+?>
