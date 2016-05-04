bbb2b0ac646275b2d9295f6e0245f29fcd1d525b
Ben <ocordu@gmail.com>
2016-05-04 04:34:56 +0100

    Fixed lalala

--- javascript/lalala.js
+++ javascript/lalala.js
@@ -1,15 +1,18 @@
-$(document).ready(function () {
-$.ajax({
-    type: "GET",
-    url: "../data/stats/course.xml",
-    dataType: "xml",
-    success: xmlParser
-});
-});
-
-$(xml).find("Statistic").each(function () {
-    if ($(this).find("Code").text() == "DEGREECLASS") {
-        $(".main").append('<div class="degree_class"><div class="type_class">' + $(this).find("Details").find("StatisticDetail").each(function() {If $(this).find("Code") == "UCONT" OUTPUT "People who continued:"}) +   '</div><div   class="percentage_class"> ' + $(this).find("Value").text() + '</div></div>');
-        //USE JS TO INSERT HTML CODE WITH DEGREE STATS
-    }
+$(document).ready(function() {
+    $.ajax({
+		type: "GET",
+		url: "data/stats/course.xml",
+		dataType: "xml"
+	}).success(function(data) {
+		$(data).find("Statistic").each(function() {
+			if ($(this).find("Code").text() == "DEGREECLASS") {
+				$(this).find("Details").find("StatisticDetail").each(function() {
+					if ($(this).find("Code") == "UCONT") {
+						var percent = $(this).find("Value").text();
+						//Now we output the HTML knowing that the percentage represents the group that continued.
+					}}
+				);
+			}
+		});
+	});
 });
