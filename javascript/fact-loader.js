dc1d5dea8a7673e9a256c5a00555fd569c85de20
Ben <ocordu@gmail.com>
2016-05-04 06:06:47 +0100

    Added comments

--- javascript/fact-loader.js
+++ javascript/fact-loader.js
@@ -1,8 +1,13 @@
 $.get("data/stats/course.xml").success(function(data) {
+	//loop through all of the Statistic nodes
 	$(data).find("Statistic").each(function(index, statistic) {
+		//if the current Statistic node's child node Code contains the text DEGREECLASS
 		if ($(statistic).children("Code").text() == "DEGREECLASS") {
+			//loop through all of this Statistic node's detail node's StatisticDetail's nodes
 			$(statistic).find("Details").find("StatisticDetail").each(function(index, statisticDetail) {
+				//if the current statistic detail's code node has text UFIRST
 				if ($(statisticDetail).find("Code").text() == "UFIRST") {
+					//$(statisticDetail).find("Value").text() is the percentage
 					$("#cs_facts").html($(statisticDetail).find("Value").text() + "% achieved a first.");
 				}}
 			);
