e05e55986d308f6babd15989d23a8cf0dba04929
Ben <ocordu@gmail.com>
2016-05-04 03:41:07 +0100

    Refactored update-stats.php

--- php/update-stats.php
+++ php/update-stats.php
@@ -1,6 +1,6 @@
 <?php
 	header("Authorization: Basic 4MTU4EHH6RKQXZYEMK04");
 	$data = file_get_contents("http://4MTU4EHH6RKQXZYEMK04:killme@data.unistats.ac.uk/api/v3/KIS/Institution/10007798/Course/242/FullTime/Statistics.XML");
-	$stats = fopen("data/stats/manchester.xml", "w");
+	$stats = fopen("data/stats/course.xml", "w");
 	fwrite($stats, $data);
 	fclose($stats);
